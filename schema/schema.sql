0deca322e300095866f3b11d74ac14924a551cdb
sukramvammaravilli <siddharthsidhu80@gmail.com>
2026-05-19 23:36:21 +0530

    COM:folder structures,env config,schema and connection

---
+++ schema/schema.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE wallet_system;
+
+USE wallet_system;
+
+CREATE TABLE accounts (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    account_number VARCHAR(50) UNIQUE NOT NULL,
+    currency VARCHAR(10) NOT NULL,
+    balance DECIMAL(18,3) DEFAULT 0.000,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE transactions (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    type VARCHAR(20) NOT NULL,
+    from_account INT NULL,
+    to_account INT NULL,
+    amount DECIMAL(18,3) NOT NULL,
+    currency VARCHAR(10) NOT NULL,
+    reference_transaction_id INT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (from_account) REFERENCES accounts(id),
+    FOREIGN KEY (to_account) REFERENCES accounts(id)
+);
